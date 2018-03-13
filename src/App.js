9bffafb51a42831dc95cfb917c9f739723ae9a2b
Eric Adams <adams.ea@gmail.com>
2018-03-12 20:45:52 -0400

    Day 1 Work Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Grid } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
 
 // Our Components
+import Navigation from './components/Navigation';
 import Home from './pages/Home';
+import Images from './pages/Images';
+import Upload from './pages/Upload';
 
 // Styles
 import './App.css';
@@ -13,7 +16,13 @@ class App extends Component {
     render() {
         return <Router>
             <Grid fluid>
+                <Row>
+                    <Navigation />
+                </Row>
+
                 <Route exact path="/" component={Home} />
+                <Route path="/images" component={Images} />
+                <Route path="/upload" component={Upload} />
             </Grid>
         </Router>
     }
