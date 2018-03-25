3057ebcdb4376d9e97673f61744e6bbd6fa0143d
Eric Adams <adams.ea@gmail.com>
2018-03-25 15:37:54 -0400

    More upload styles

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -4,13 +4,17 @@ import {
     Col,
     FormGroup,
     FormControl,
-    HelpBlock
+    HelpBlock,
+    Image
 } from 'react-bootstrap';
 import axios from 'axios';
 
 import MemeCreator from '../components/MemeCreator';
 
+import './Upload.css';
+
 const IMAGES_API = 'https://kpm2qrhrf8.execute-api.us-east-1.amazonaws.com/dev/images';
+const MEMES_API = 'https://kpm2qrhrf8.execute-api.us-east-1.amazonaws.com/dev/memes';
 
 export default class Upload extends Component {
 
